fd909a9f04e1af2653e1957865e4c03f1d03c1c9
Christia Tidwell <christiatidwell@bellsouth.net>
2018-03-24 15:03:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,3 +11,38 @@ def display_board(board)
 end
 
 display_board(board)
+
+board = ["   "," X "," O "]
+
+def display_board(board)
+  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+  puts "-----------"
+  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+  puts "-----------"
+  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+end
+
+display_board(board)
+
+
+board = ["   "," X "," O "]
+
+def display_board(board)
+  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+  puts "-----------"
+  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+  puts "-----------"
+  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+end
+
+display_board(board)
+
+def display_board(board)
+  puts "#{board[2]}|#{board[0]}|#{board[0]}"
+  puts "-----------"
+  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+  puts "-----------"
+  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+end
+
+display_board(board)
