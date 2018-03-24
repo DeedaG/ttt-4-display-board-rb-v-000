c270e141f7f8b706affdb7d8416e8c40f1b4361b
Christia Tidwell <christiatidwell@bellsouth.net>
2018-03-24 14:56:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 
 board = ["   "," X "," O "]
 
-def display_board(board[0])
+def display_board(board)
   puts "#{board}|#{board}|#{board}"
   puts "-----------"
   puts "#{board}|#{board}|#{board}"
