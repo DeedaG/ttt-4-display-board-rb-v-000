c698ce3f751b4466322edecc257256aa56a82863
Christia Tidwell <christiatidwell@bellsouth.net>
2018-03-24 14:44:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,3 +2,16 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
 
+def game
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def display_board
+  game
+end
+
+display_board
