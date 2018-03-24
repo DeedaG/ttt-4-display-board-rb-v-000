2d23027f3108dc0418e36fbd29ad4c7e10a3a015
Christia Tidwell <christiatidwell@bellsouth.net>
2018-03-24 15:14:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,48 +1,17 @@
 
 
-board = 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board(board)
 
-board = ["   "," X "," O "]
-
-def display_board(board)
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
-  puts "-----------"
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
-  puts "-----------"
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
-end
-
-display_board(board)
-
-
-board = ["   "," X "," O "]
-
-def display_board(board)
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
-  puts "-----------"
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
-  puts "-----------"
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
-end
-
-display_board(board)
-
-def display_board(board)
-  puts "#{board[2]}|#{board[0]}|#{board[0]}"
-  puts "-----------"
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
-  puts "-----------"
-  puts "#{board[0]}|#{board[0]}|#{board[0]}"
-end
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
 display_board(board)
